dda8178e25863b964d022afbf8fc30ec959ffc53
riffs2005 <kc@Keiths-MacBook-Pro-2.local>
2014-06-15 05:43:25 -0700

    adding guest
    Guest can be added.
    6 character random key is generated for GID.
    Assertion needs more work. Curretly this only checks for empty field
    for now.

--- KBIT---CPSC-304-Database/add-guest.php
+++ KBIT---CPSC-304-Database/add-guest.php
@@ -1,10 +1,36 @@
+<?php
+
+include 'idGen.php';
+include 'connect.php';
+include 'sqlFunction.php';
+
+
+if (empty($_POST["firstName"]) || empty($_POST["lastName"]) || empty($_POST["extraGuests"]) || empty($_POST["email"]) || empty($_POST["emailRe"]))
+	echo "<SCRIPT>alert('All fields must not be empty.');</SCRIPT>";
+else 
+{
+	$cmdstr = "INSERT INTO Guest (GID, name, maxnumberallowed, numberBringing) VALUES ('".
+	idGen()."', '".
+	$_POST["firstName"]." ".$_POST["lastName"]."', ".
+	"0, ".
+	$_POST["extraGuests"].")";
+	echo "Guest added successfully<br><br>command executed : ".$cmdstr;
+
+	executePlainSQL($cmdstr);
+	OCICommit($db_conn);
+}
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
   </head>
 <body>
 		<h1>Add Guest</h1>
-		<form role="form" action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+		<form role="form" action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
 		<div class="form-group">
 			<label for="firstName">First Name</label>
 			<input type="text" class="form-control" name="firstName" placeholder="First Name">
@@ -18,26 +44,17 @@
 			<input type="number" class="form-control" name="extraGuests" placeholder="#" size="2">
 		</div>
 		<div class="form-group">
-			<label for="password">New Password</label>
-			<input type="password" class="form-control" name="password" placeholder="Password">
+			<label for="email">Email</label>
+			<input type="email" class="form-control" name="email" placeholder="Email">
 		</div>
 		<div class="form-group">
-			<label for="passwordRetype">Re-Type Password</label>
-			<input type="password" class="form-control" name="passwordRe" placeholder="Re-Type Password">
+			<label for="emailRe">Re-Type Email</label>
+			<input type="email" class="form-control" name="emailRe" placeholder="Re-Type Email">
 		</div>
 		<button type="submit" class="btn btn-default">
 			Add
 		</button>			
 	</form>
 
-	<?php
-	echo "First Name: ", $_POST["firstName"];
-	echo "<br>";
-	echo "Last Name: ", $_POST["lastName"];
-	echo "<br>";
-	echo "Number of guests: ", $_POST["extraGuests"];
-	echo "<br>";
-	echo crypt($_POST["password"]);
-	?>
-  </body>
+</body>
 </html>

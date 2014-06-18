2c4da1541f6b55ec90a7df8b78f1c8d7b063f2e7
riffs2005 <kc@dhcp-128-189-248-170.ubcsecure.wireless.ubc.ca>
2014-06-18 12:02:22 -0700

    Financial Reports
    Cost broken down according to
     -Item
     -Item/Venue
     -Item/Vendor

---
+++ KBIT---CPSC-304-Database/report-cost-vendor.php
@@ -0,0 +1,49 @@
+				<?php
+				$queryString = "CREATE VIEW VendorTCost AS 
+								SELECT sp.companyName, sp.itemName, sp.providedNumber * sq.unitCost AS totalCost
+								FROM SupplyProvided sp, SupplyQuoted sq
+								WHERE sp.gID = sq.gID AND sp.itemName = sq.itemName
+								ORDER BY companyName Desc, itemName Desc";
+				$queryStringPrint = "SELECT * FROM VendorTCost";
+				$queryStringSum = "SELECT SUM(totalCost) 
+								            FROM VendorTCost";
+				$queryStringEachVendorSum = "SELECT companyName, SUM(totalCost) 
+								            FROM VendorTCost
+								            GROUP BY companyName";
+				$queryStringPost = "DROP VIEW VendorTCost";
+
+				executePlainSQL($queryString);
+				OCICommit($db_conn);
+				$Result = executePlainSQL($queryStringPrint);
+				echo "<table id=\"3-1m\" width=100%><tr><th>Vendor</th><th>Item</th><th>TotalCost</th></tr>";
+				while ($row = OCI_Fetch_Array($Result, OCI_NUM))
+				{
+					echo "<tr><td>".$row[0]."</td>";
+					echo "<td>".$row[1]."</td>";
+					echo "<td>".$row[2]."</td></tr>";
+				}
+				$Result = executePlainSQL($queryStringSum);
+				while ($row = OCI_Fetch_Array($Result, OCI_NUM))
+				{
+					echo "<tr><td>Total</td><td></td><td><h2>".$row[0]."</h2></td></tr>";
+				}
+				echo "</table>";
+
+
+				$Result = executePlainSQL($queryStringEachVendorSum);
+				echo "<table id=\"3-2m\" width=100%><tr><th>Vendor</th><th></th><th>TotalCost</th></tr>";
+				while ($row = OCI_Fetch_Array($Result, OCI_NUM))
+				{
+					echo "<tr><td>".$row[0]."</td><td></td>";
+					echo "<td>".$row[1]."</td></tr>";
+				}
+				$Result = executePlainSQL($queryStringSum);
+				while ($row = OCI_Fetch_Array($Result, OCI_NUM))
+				{
+					echo "<tr><td>Total</td><td></td><td><h2>".$row[0]."</h2></td></tr>";
+				}
+				echo "</table>";
+
+				$Result = executePlainSQL($queryStringPost);
+				OCICommit($db_conn);
+				?>
